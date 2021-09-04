7e8fbba13996aa42932f6393d78e933be3f8a2ef
kirankr24 <89823556+kirankr24@users.noreply.github.com>
2021-09-04 17:09:43 +0530

    updated code

--- pd_1_w1_funct.py
+++ pd_1_w1_funct.py
@@ -1,3 +1,10 @@
-def addnum(x,y,z):
-    return x+y+z
-print(addnum(1,2,3))
+def addnum(x,y,z=None, flag="False"):
+    if (flag):
+        print("It's true flag")
+    if (z==None):
+        return x + y
+    else :
+        return x+y+z
+
+
+print(addnum(3,2,flag="True"))
