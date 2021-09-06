c0f7b0edbaf1e8e61a09584af0fec9a9b9f67e3f
kirankr24 <89823556+kirankr24@users.noreply.github.com>
2021-09-06 09:09:55 +0530

    updated

---
+++ pd_1_w1_splitanddictionaory.py
@@ -0,0 +1,53 @@
+fn = "hello"
+ln = "world"
+
+print(fn + " " + ln)
+print(fn*3)
+print("hel" in fn)
+
+
+fn = "Hello world iam macbook".split(" ")[0]
+ln = "Okay universe iam computer".split(" ")[3]
+print(fn, ln)
+
+print("-------------")
+# dictionary part
+
+d = { "kiran" : "kirankrch24@gmail.com", "kiran sec" : "usemymail11@gmail.com" }
+d["kiran 3rd"] = "gmail.com"
+
+for i,o in d.items():
+    print(i)
+    print(o)
+
+print("-------------")
+# example of unpacking 
+
+x = ("kiran", "kiran sec", "kiran 3rd")
+name, sec, rd = x
+
+print(name, rd, sec)
+
+
+
+print("-------------Formatting")
+# formatting 
+
+sales_record = { 
+    'price' : 3.24,
+    'num_item' : 4,
+    'person' : 'chris'
+
+}
+
+sales_state = ' {} brought {} items at {} price each '
+
+print(sales_state.format(sales_record['person'],sales_record['num_item'], sales_record['price']))
+
+
+
+
+
+#1 : Hello , respected Sir / Madam i am Kiran Kumar Chaudhary right now i am pursed Bachelor’s of Engineering in Electronics and Communication Engineering ( ECE ) from Vishwakarma Government Engineering College ( VGEC ), Ahmedabad , Gujarat , India and due my poor academic in technical subject (i.e i failed in many subjects ) i got detained for two year in my college, i want to change my domain to Data Science . I earn some amount of money by teaching 11th-12th student in tuition to pay rent of house by my own . As a matter of fact it is a part time earning . I think this course help to change my domain to Data Science. I am studied in Electronics and Communication Engineering ( ECE ) field and I also want to pursue my education in Data Science in graduate level. I interested in keep contact with this community and want to make contribution along with them. But I have no job of my own to carry the expanses to pay for the certificate of this course . In this circumstance , it is very much difficult for me to gather such amount of money for the certificate . So I am in need of this financial aid . I hope you will give me financial aid.
+#2 : This course help me more to achieve my professional in internship / research in field of Data Science. With the help of this course i will able to achieve 100 points in Vishwakarma Government Engineering College ( VGEC ) which is afflicted to Gujarat Technological University( GTU ). This certificate help me to get research assistant position or internship in Indian Institute of Technology ( IITs ) / Indian Institute of Science ( IISc ) . In graduate research level, this certificate will be valuable in my higher study education regarding in this field and I want to study in Data Science field in future. It will help me to get job in Data Science companies or it will help to accelerate my educational career in Data Science. Today Data Science course certificate is more valuable for me to make my career in Data driven industry. If you are able to give me chance to get  certified certificate of course Python Data Science ( from UNIVERSITY OF Michigan ) if will very much help to me for my career. I hope you will give me the course content and certified certificate for free after seeing my financial conditional of me.
+#3 : I can't afford as of now.
